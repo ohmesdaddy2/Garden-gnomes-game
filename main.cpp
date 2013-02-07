b622b7a802ef132cbc8e07ca9e8bb810a80b5f64
ohmesdaddy2 <amngco@yahoo.com>
2013-02-06 18:41:45 -0600

    added a few basic includes with no comments yet

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: A mean Gecko
+ *
+ * Created on February 6, 2013, 6:40 AM
+ */
+#include <cstdlib>
+#include "gameengine.h"
+
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    
+    return 0;
+}
+
